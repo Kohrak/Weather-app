8eb2c36b8452b8ce5923944084bc2d9d41d6e760
Kohrak <gabrielgt95@gmail.com>
2018-01-18 23:12:53 +0000

    create a setIcon function to handle the output to the html

--- weather.js
+++ weather.js
@@ -1,19 +1,21 @@
 
 function getWeather(){
-  navigator.geolocation.getCurrentPosition(function(position){
+  navigator.geolocation.getCurrentPosition(function handlePosition(position){
     var lat = position.coords.latitude
     var lon = position.coords.longitude
     var url = "http://api.openweathermap.org/data/2.5/weather?APPID=2df87e789845393d6e88674f59d36941&lat=" + lat + '&lon=' + lon;
-    console.log(url)
-    //var url = "http://api.openweathermap.org/data/2.5/weather?lat=51.51&lon=-0.13&APPID=2df87e789845393d6e88674f59d36941"
     var xhr = new XMLHttpRequest();
     xhr.open("GET", url);
     xhr.send();
-    xhr.onload = function() {
+    xhr.onload = function handleRequest() {
       var weatherData = JSON.parse(xhr.responseText);
-      console.log(weatherData.weather[0].main);
+      setIcon(weatherData.weather[0].main);
     }
   });
 }
 
 getWeather();
+
+function setIcon(currentWeather){
+  console.log(currentWeather)
+}
