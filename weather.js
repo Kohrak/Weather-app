27b3e04b308d5119b3f95cfc316ee44eb0543c7d
Kohrak <gabrielgt95@gmail.com>
2018-01-18 22:24:47 +0000

    create a lot of mess, xhr call working

--- weather.js
+++ weather.js
@@ -4,12 +4,41 @@ function success(pos) {
   console.log('Latitude : ' + crd.latitude);
   console.log('Longitude: ' + crd.longitude);
   console.log('More or less ' + crd.accuracy + ' meters.');
+  return {"latitude": crd.latitude, "longitude": crd.longitude }
 };
 
 function error(err) {
   console.warn('ERROR(' + err.code + '): ' + err.message);
 };
 
-navigator.geolocation.getCurrentPosition(success, error);
 
-var url = 'api.openweathermap.org/data/2.5/weather?&APPID=2df87e789845393d6e88674f59d36941&lat=' + lat + '&lon=' + lon + "&callback=mycallback";
+
+
+function jsonpRequest(){
+  //navigator.geolocation.getCurrentPosition(function(position){
+    //var lat = position.coords.latitude
+    //var lon = position.coords.longitude
+    //var tag = document.createElement("script");
+    //var url = 'api.openweathermap.org/data/2.5/weather?&APPID=2df87e789845393d6e88674f59d36941&lat=' + lat + '&lon=' + lon + "&callback=mycallback";
+    //var url = "api.openweathermap.org/data/2.5/weather?lat=51.51&lon=-0.13&APPID=2df87e789845393d6e88674f59d36941"
+    //tag.src = url;
+    //tag.id = "jsonpCall"
+    //document.getElementsByTagName("head")[0].appendChild(tag);
+    var data = null;
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "http://api.openweathermap.org/data/2.5/weather?lat=51.51&lon=-0.13&APPID=2df87e789845393d6e88674f59d36941");
+xhr.send();
+xhr.onload = function() {
+  var weatherData = JSON.parse(xhr.responseText)
+  console.log(weatherData.weather[0].main)
+}
+  //});
+}
+/*
+function JSONP_CALLBACK(data) {
+  console.log(data);
+  document.getElementsByTagName("head")[0].removeChild(document.getElementById("jsonpCall"));
+}*/
+
+jsonpRequest();
