aadb43a33e90dd3e1122409166653e56139ea8c0
Kohrak <gabrielgt95@gmail.com>
2018-01-22 18:00:12 +0000

    add unit parameter for temperature

--- weather.js
+++ weather.js
@@ -1,16 +1,16 @@
 
-function getWeather(){
+function getWeather(unit){
   navigator.geolocation.getCurrentPosition(function handlePosition(position){
     var lat = position.coords.latitude
     var lon = position.coords.longitude
-    var url = "http://api.openweathermap.org/data/2.5/weather?APPID=2df87e789845393d6e88674f59d36941&lat=" + lat + '&lon=' + lon;
+    var url = "http://api.openweathermap.org/data/2.5/weather?APPID=2df87e789845393d6e88674f59d36941&lat=" + lat + '&lon=' + lon + "&units=" + unit;
     var xhr = new XMLHttpRequest();
     xhr.open("GET", url);
     xhr.send();
     xhr.onload = function handleRequest() {
       var weatherData = JSON.parse(xhr.responseText);
       setIcon(weatherData.weather[0].id, weatherData.weather[0].description);
-      setTemp(weatherData.main.temp);
+      setTemp(weatherData.main.temp, unit);
     }
   });
 }
@@ -48,8 +48,16 @@ function setIcon(id, description){
   }
 }
 
-function setTemp(temperature){
-  document.getElementById("temp").innerHTML = temperature;
+function setTemp(temperature, unit){
+switch (unit){
+  case "metric":
+    var units = "C";
+    break;
+  case 'imperial':
+    var units = "F";
+    break;
+  }
+  document.getElementById("temp").innerHTML = temperature + " " + units;
 }
 
-getWeather();
+getWeather('metric');
