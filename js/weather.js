72f2f8e702700fb3f505bb53ed2854ad4c69d839
Kohrak <gabrielgt95@gmail.com>
2018-01-27 12:42:15 +0000

    add s to http on api call

--- js/weather.js
+++ js/weather.js
@@ -5,7 +5,7 @@ function getWeather(){
   navigator.geolocation.getCurrentPosition(function handlePosition(position){
     var lat = position.coords.latitude
     var lon = position.coords.longitude
-    var url = "http://api.openweathermap.org/data/2.5/weather?APPID=2df87e789845393d6e88674f59d36941&lat=" + lat + '&lon=' + lon + "&units=metric";
+    var url = "https://api.openweathermap.org/data/2.5/weather?APPID=2df87e789845393d6e88674f59d36941&lat=" + lat + '&lon=' + lon + "&units=metric";
     var xhr = new XMLHttpRequest();
     xhr.open("GET", url);
     xhr.send();
